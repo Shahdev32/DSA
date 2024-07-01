be3360b6fbe799c9bbe3df0ee68371ad4eaedc85
shahdev kumar <shahdevkumar.d@gmail.com>
2024-07-01 23:07:42 +0500

    multiplicaiton

---
+++ Testarray01.java
@@ -0,0 +1,7 @@
+public class Testarray01 {
+  public static void main(String[] args) {
+    int a[] = {23,4,1,5};
+    for(int i:a)
+    System.out.println(i);
+  }   
+}
---
+++ Testarray1.java
@@ -0,0 +1,9 @@
+public class Testarray1 {
+   public static void main(String[] args) {
+    int a[]= {22,31,4};
+    for(int i =0;i<a.length;i++){
+    System.out.println(a[i]);
+
+    } 
+   }
+}
